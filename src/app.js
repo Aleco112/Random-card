52b96c09ffd0e7b70063e5d7176419199ea95569
Aleco112 <aleco112@yahoo.fr>
2022-03-05 02:03:37 +0000

    random-card

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const suits = ["&spades;", "&hearts;", "&diams;", "&clubs;"];
+undefined;
+const numbers = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function getRandom(min, max) {
+  return Mathrandom()(max - min) + min;
+}
+function getRandomNumber(min, max) {
+  return Math.random()(max - min) + min;
+}
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
