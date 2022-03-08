01ba6c039bbb4cb506a48654a344d9728c4e50f6
Aleco112 <aleco112@yahoo.fr>
2022-03-08 02:00:18 +0000

    new one

--- src/app.js
+++ src/app.js
@@ -1,27 +1,38 @@
-const suits = ["&spades;", "&hearts;", "&diams;", "&clubs;"];
-undefined;
-const numbers = [
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K",
-  "A"
-];
+import "./style.css";
+function functionToRun() {
+  const suits = ["&spades;", "&hearts;", "&diams;", "&clubs;"];
+  const numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
 
-function getRandom(min, max) {
-  return Mathrandom()(max - min) + min;
+  function randomNumber(min, max) {
+    const randomFloat = Math.random();
+    return Math.floor(randomFloat * (max - min) + min);
+  }
+
+  const suitsIndex = randomNumber(0, suits.length - 1);
+  const numberIndex = randomNumber(0, numbers.length - 1);
+
+  const suitTop = document.querySelector(".suits.top");
+  const number = document.querySelector(".number");
+  const suitBottom = document.querySelector(".suits.bottom");
+
+  number.innerHTML = numbers[numberIndex];
+  suitTop.innerHTML = suits[suitsIndex];
+  suitBottom.innerHTML = suits[suitsIndex];
 }
-function getRandomNumber(min, max) {
-  return Math.random()(max - min) + min;
-}
-function getRandomNumber(min, max) {
-  return Math.random() * (max - min) + min;
-}
+window.onload = function() {
+  functionToRun();
+};
